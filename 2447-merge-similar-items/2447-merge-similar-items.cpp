206c5fdf5de66ca82e97ebec74838a29d9932d7b
Deeksha Kushwaha <162332494+Deeks779@users.noreply.github.com>
2025-07-02 22:49:25 +0530

    Time: 15 ms (13.69%), Space: 21.8 MB (24.96%) - LeetHub

---
+++ 2447-merge-similar-items/2447-merge-similar-items.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        map<int, int> m;
+
+        for (auto i : items1) {
+            m[i[0]] += i[1];
+        }
+
+        for (auto i : items2) {
+            m[i[0]] += i[1];
+        }
+
+        vector<vector<int>> res;
+        for (auto [value, weight] : m) {
+            res.push_back({value, weight});
+        }
+
+        return res;
+    }
+};

2408b2abc07b1b8945cd54138c09f5a6431999f6
Deeksha Kushwaha <162332494+Deeks779@users.noreply.github.com>
2026-01-05 17:09:34 +0530

    Time: 3 ms (46.23%), Space: 39.2 MB (25.94%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -6,15 +6,12 @@ public:
         int count=0;
         long long smallest=LLONG_MAX;
         for(int i=0;i<n;i++){
-            long long sum=0;
             for(int j=0;j<n;j++){
                 long long num=abs(matrix[i][j]);
                 smallest=min(smallest,num);
                 if(matrix[i][j]<0) count++;
-                sum+=num;
+                minSum+=num;
             }
-            cout<<sum<<" "<<minSum<<" "<<smallest<<endl;
-            minSum+=sum;
         }
         if(count%2!=0) minSum-=(2*smallest);
         return minSum;
